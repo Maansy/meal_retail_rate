94df324e6a03b7ddd917dfc8b48149cce0cf6bca
Mansy <abdomansy19@gmail.com>
2023-09-03 20:35:59 +0300

    dd

--- project/project/settings.py
+++ project/project/settings.py
@@ -134,10 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
