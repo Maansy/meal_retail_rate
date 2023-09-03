87253df4312440659bcbc6dd2bf9af70c754bf83
Mansy <abdomansy19@gmail.com>
2023-09-03 20:48:05 +0300

    dd

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -11,7 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
 application = Cling(get_wsgi_application())
+# application = WhiteNoise(application)
